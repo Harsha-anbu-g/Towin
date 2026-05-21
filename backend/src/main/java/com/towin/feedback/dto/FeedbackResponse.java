e33adc4dda8efa4c5163e413607a008c943b0b2d
Harshavardhan ag <agharsha.anbu@gmail.com>
2026-05-20 23:30:52 -0400

    fix: apply code quality improvements to feedback feature

--- backend/src/main/java/com/towin/feedback/dto/FeedbackResponse.java
+++ backend/src/main/java/com/towin/feedback/dto/FeedbackResponse.java
@@ -1,11 +1,13 @@
 package com.towin.feedback.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data @Builder
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class FeedbackResponse {
     private UUID id;
     private String name;
